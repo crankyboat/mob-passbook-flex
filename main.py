7848e8af9715c8c8906b803c6fbc2f0fedd4d75e
Liao <k-liao@Kestrels-MBP.Mobivity>
2016-08-24 16:19:39 -0700

    Added support to accept offer name, text, and image url; expiration date needs fixing.

--- main.py
+++ main.py
@@ -14,10 +14,18 @@
 
 # [START app]
 import logging
+import datetime
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
 
 from flask import Flask, render_template, send_from_directory, send_file, request
-from passbook.models import Pass, StoreCard, Barcode, BarcodeFormat
+from passbook.models import Pass, Coupon, Barcode, BarcodeFormat
 from uuid import uuid4
+from urllib import urlopen
+from pyzipcode import ZipCodeDatabase
 
 
 app = Flask(__name__)
@@ -30,17 +38,46 @@ def hello():
 @app.route('/pass')
 def send_pass():
 
+    # User info
     uid = request.args.get('uid')
     fname = request.args.get('fname')
     lname = request.args.get('lname')
+
+    # Context info
+    zipcode = request.args.get('zipcode')
+    zcdb = ZipCodeDatabase()
+    timezone = zcdb[zipcode].timezone + 1 #HACK yet to fix timezone
+
+    class tzfromzip(datetime.tzinfo):
+        _offset = datetime.timedelta(hours=timezone)
+        _dst = datetime.timedelta(hours=1)
+        _name = str(timezone)
+        def utcoffset(self, date_time):
+            return self.__class__._offset
+        def dst(self, date_time):
+            return self.__class__._dst
+        def tzname(self, date_time):
+            return self.__class__._name
+        def utcoffset(self, date_time):
+            return self.__class__._offset
+
+    # Offer info
+    offername = request.args.get('offerName')
+    offertext = request.args.get('offerText')
+    offerimg = request.args.get('offerImage')
+    offerexp = request.args.get('offerExpiration')
+    offerexp_int = [int(s) for s in offerexp.split('/')]
+    offerexpdt = datetime.datetime(offerexp_int[0], offerexp_int[1], offerexp_int[2], 23, 59, 59, 0, tzinfo=tzfromzip()).isoformat('T')
+
     msg = fname + ' ' + lname + '\n'
-    msg += 'Get a free 6" Sub when you buy a 30oz drink\n'
+    msg += offertext + '\n'
     msg += uid + '\n'
     msg += '6" Meatball Sandwich\n'
-    msg += '8/9/16\n'
+    msg += offerexp + '\n'
 
-    cardInfo = StoreCard()
-    cardInfo.addPrimaryField('name', 'Free Food', 'Free food at Mobivity SD!')
+    cardInfo = Coupon()
+    cardInfo.addPrimaryField('offer', offername, offertext)
+    cardInfo.addAuxiliaryDateField('expires', offerexpdt, 'EXPIRES')
 
     organizationName = 'Mobivity'
     passTypeIdentifier = 'pass.com.mobivity.scannerdemo'
@@ -51,6 +88,8 @@ def send_pass():
                     organizationName=organizationName, \
                     teamIdentifier=teamIdentifier)
     passfile.serialNumber = uuid4().hex
+
+    # Customize the pass
     passfile.barcode = Barcode(message=msg, format=BarcodeFormat.QR)
     passfile.foregroundColor = 'rgb(255, 255, 255)'
     passfile.backgroundColor = 'rgb(72,158,59)'
@@ -58,6 +97,7 @@ def send_pass():
     # Including the icon and logo is necessary for the passbook to be valid.
     passfile.addFile('icon.png', open('static/images/pass/icon.png', 'r'))
     passfile.addFile('logo.png', open('static/images/pass/logo.png', 'r'))
+    passfile.addFile('strip.png', StringIO(urlopen(offerimg).read()))
 
     # Create and output the Passbook file (.pkpass)
     pkpass = passfile.create('static/cert/certificate.pem', 'static/cert/key.pem', 'static/cert/wwdr.pem', '')
